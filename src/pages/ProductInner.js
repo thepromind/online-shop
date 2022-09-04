1a4bde8552d61629e9eb17965ea59a50e171259a
verilog <z.kalkayev@jysanbank.kz>
2022-09-05 02:22:36 +0600

    Six commit

---
+++ src/pages/ProductInner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function ProductInner() {
+return (
+    <main>
+        <div className="container-fluid">
+            <Banner/>
+        </div>
+    </main>
+);
+}
+
+export default ProductInner;
